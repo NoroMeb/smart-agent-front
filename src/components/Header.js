e7d427e3fff4c86fc3a2acac90b8c0c0f6ef6513
Noro <noromeb85@gmail.com>
2023-02-10 16:03:53 +0100

    added buttons

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,19 @@
-import { useEthers } from '@usedapp/core';
-
+import { Link } from "react-router-dom";
 function Header() {
-  const { activateBrowserWallet, account, deactivate } = useEthers()
+
   return (
     <header className='header'>
       <div class="logo">
-        <h1>Smart Agent</h1>
+        <h1 className="logo-text">Smart</h1>
+        <h1 className="logo-text">Agent</h1>
       </div>
-      {!account && <button className='connect' onClick={() => activateBrowserWallet()}>Connect</button>}
-      {account && <button className='connect' onClick={deactivate}>{account}</button>}
+      <div className="buttons">
+        <button className='how-it-works'>Fund us</button>
+        <button className='how-it-works'>Whitepaper</button>
+        <button className='how-it-works'>Smart contract</button>
+        <button className='how-it-works'>How it works</button>
+        <Link to={"/dashboard"} target="_blank"><button className='launch-app' >Start App</button></Link>
+      </div>
     </header>
   );
 }
