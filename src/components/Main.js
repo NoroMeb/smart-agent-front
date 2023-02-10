438a404666df1c1d4032d9c16245684be0742f10
Noro <noromeb85@gmail.com>
2023-02-10 16:04:08 +0100

    added Main component

---
+++ src/components/Main.js
@@ -0,0 +1,15 @@
+import React from "react";
+function Main() {
+
+    return (
+        <main className='main'>
+            <div class="main-text">
+                <h1 className="main-h1">A trust-less way to collaborate</h1>
+                <h3>in your Youtube Videos</h3>
+            </div>
+
+        </main>
+    );
+}
+
+export default Main;
