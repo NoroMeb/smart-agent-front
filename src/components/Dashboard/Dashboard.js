c972c333b445445839821c310de424ba97e3406c
Noro <noromeb85@gmail.com>
2023-02-11 16:13:20 +0100

    added dashboard

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,28 +1,10 @@
-import { useEthers, account, activateBrowserWallet, deactivate } from "@usedapp/core";
-import { Button } from "@mui/material";
-import { createTheme } from '@mui/material/styles';
-import { purple } from "@mui/material/colors";
+import Header from "./Header";
 function Dashboard() {
-    const { account, activateBrowserWallet, deactivate } = useEthers()
-
-    const theme = createTheme({
-        palette: {
-            primary: {
-                main: '#15E700',
-            },
-            secondary: {
-                main: '#15E700',
-            },
-        },
-    });
     return (
         <div>
-            <div>
-                {account && <Button variant="contained" onClick={deactivate} >{account}</Button>}
-                {!account && <Button variant="contained" onClick={() => activateBrowserWallet()} size="small">Connect</Button>}
-            </div>
+            <Header />
         </div>
-    )
+    );
 }
 
 export default Dashboard;
