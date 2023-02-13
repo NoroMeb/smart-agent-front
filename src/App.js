0f4448a2e984720eaced67edb35fc000baad066f
Noro <noromeb85@gmail.com>
2023-02-13 16:13:11 +0100

    fixed routes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import { useEthers, DAppProvider } from '@usedapp/core';
-import { Routes, Route } from 'react-router-dom';
-import Navbar from "./components/Home/Navbar";
-import Main from './components/Home/Main';
+import Home from './components/Home/Home';
 import Dashboard from './components/Dashboard/Dashboard';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Videos from './components/Dashboard/pages/Videos';
+import Collaborations from './components/Dashboard/pages/Collaborations'
+import Test from './components/Test';
 
 function App() {
   return (
     <DAppProvider config={{}}>
-      <div>
-        <Routes>
-          <Route path='/' element={<div className='home'><Navbar /><Main /></div>} />
-          <Route path='/dashboard' element={<div><Dashboard /></div>} />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/test' element={<Test />} />
+        <Route path="dashboard" element={<Dashboard />}>
+          <Route path="videos" element={<Videos />} />
+          <Route path="collaborations" element={<Collaborations />} />
+        </Route>
+      </Routes>
 
-        </Routes>
-
-      </div>
     </DAppProvider>
   );
 }
