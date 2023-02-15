dcdfb654c0bf9f46ecff42044456ee48a97629e5
Noro <noromeb85@gmail.com>
2023-02-15 16:28:27 +0100

    added new collaboration page route

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import { useEthers, DAppProvider } from '@usedapp/core';
 import Home from './components/Home/Home';
 import Dashboard from './components/Dashboard/Dashboard';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import AddCollaboration from './components/Dashboard/pages/AddCollaboration';
+import Collaborations from './components/Dashboard/pages/Collaborations';
 import Videos from './components/Dashboard/pages/Videos';
-import Collaborations from './components/Dashboard/pages/Collaborations'
 import Test from './components/Test';
 
 function App() {
@@ -13,8 +14,9 @@ function App() {
         <Route path='/' element={<Home />} />
         <Route path='/test' element={<Test />} />
         <Route path="dashboard" element={<Dashboard />}>
-          <Route path="videos" element={<Videos />} />
+          <Route path="add-collaboration" element={<AddCollaboration />} />
           <Route path="collaborations" element={<Collaborations />} />
+          <Route path="videos" element={<Videos />} />
         </Route>
       </Routes>
 
