e0967f08756e91d7ab99d860856b389c14ab1477
Noro <noromeb85@gmail.com>
2023-02-17 17:14:35 +0100

    passed account through props

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,29 +1,20 @@
 import Header from "./Header/Header";
 import LeftBar from "./LeftBar/LeftBar";
 import { Outlet } from 'react-router-dom';
-import LeftBarTest from "./LeftBar/LeftBarTest";
 import { useState } from "react";
-
 function Dashboard() {
 
     const [account, setAccount] = useState();
     function updateMyProp(value) {
         setAccount(value);
     }
-    console.log(account);
+    // console.log(account);
+
+
     return (
-        // <div>
-        //     <Header />
-        //     <LeftBar />
-        //     <Outlet />
-        // </div>
 
-
-
-        // <div class="container-fluid text-center">
         <div class="container-fluid ">
             <div class="row max-width" >
-                {/* <Header /> */}
                 <Header updateMyProp={updateMyProp} />
             </div>
             <div class="row max-width" style={{ backgroundColor: "#f68c8a", height: "100vh" }}>
@@ -32,7 +23,7 @@ function Dashboard() {
                 </div>
 
                 <div class="col-12 col-lg ms-auto main-scroll d-flex align-items-center justify-content-center" >
-                    <Outlet />
+                    <Outlet context={account} />
                 </div>
             </div>
         </div >
