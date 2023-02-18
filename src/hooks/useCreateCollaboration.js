d2551e7fa4067c4e892051322589b04cacca8ae2
Noro <noromeb85@gmail.com>
2023-02-18 15:02:47 +0100

    fixed useCreateCollaboration hook

--- src/hooks/useCreateCollaboration.js
+++ src/hooks/useCreateCollaboration.js
@@ -1,12 +1,15 @@
-import { utils } from 'ethers'
-import { Contract } from '@ethersproject/contracts'
-import PaidPromotion from "./../chain-info/contracts/PaidPromotion.json"
+import { ethers } from 'ethers';
+import { Contract } from '@ethersproject/contracts';
+import PaidPromotion from "./../chain-info/contracts/PaidPromotion.json";
 import { useContractFunction } from "@usedapp/core";
-export function useCreateCollaboration() {
+
+
+
+export const useCreateCollaboration = () => {
 
     const paidPromotionAddress = "0x30401029AfD9f2835260886c2E1aF2359ff77d4C"
-    const { paidPromotionAbi } = PaidPromotion
-    const paidPromotionInterface = new utils.Interface(paidPromotionAbi)
+    const { abi } = PaidPromotion
+    const paidPromotionInterface = new ethers.utils.Interface(abi)
     const paidPromotionContract = new Contract(paidPromotionAddress, paidPromotionInterface)
     const { state, send } = useContractFunction(paidPromotionContract, "startACollab", {});
     return { state, send };
