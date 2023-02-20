f513e4d22f7e00a124d74942a71e901a11a5f5fa
Noro <noromeb85@gmail.com>
2023-02-20 16:25:32 +0100

    updated the SmartContract syntax highlight

--- src/components/Dashboard/pages/SmartContract.js
+++ src/components/Dashboard/pages/SmartContract.js
@@ -1,11 +1,12 @@
-import 'highlight.js/styles/monokai-sublime.css';
-
+import SyntaxHighlighter from 'react-syntax-highlighter';
+import { tomorrowNightBright } from 'react-syntax-highlighter/dist/esm/styles/hljs';
 import { useState, useEffect } from 'react';
 import contract from "./../../../chain-info/contracts/PaidPromotion.txt"
 
 function SmartContract() {
-    var hljs = require('highlight.js/lib/core.js');
-    var hljsDefineSolidity = require('highlightjs-solidity');
+
+
+
 
 
 
@@ -18,26 +19,15 @@ function SmartContract() {
         console.log(textFileContent)
     }, []);
 
-    hljsDefineSolidity(hljs);
-    hljs.initHighlightingOnLoad();
+
+
+
+
     return (
-        <div style={{ width: '800px', overflow: 'auto', maxHeight: '36rem', whiteSpace: 'pre-wrap', marginTop: "66px" }}>
-            {/* <code class="language-solidity hljs" data-lang="solidity">
-                <span class="hljs-comment">// contracts/MyNFT.sol</span><br />
-                <span class="hljs-comment">// SPDX-License-Identifier: MIT</span><br /><br />
-
-                <span class="hljs-meta">
-                    <span class="hljs-keyword">pragma</span>
-                    <span class="hljs-keyword">solidity</span>
-                    "^0.8.0;"
-                </span>
-
-            </code> */}
-            <pre class="highlightjs highlight" >
-                <code class="language-solidity hljs" data-lang="solidity">
-                    {textFileContent}
-                </code>
-            </pre>
+        <div className='code' >
+            <SyntaxHighlighter language="java" style={tomorrowNightBright}>
+                {textFileContent}
+            </SyntaxHighlighter>
         </div >
     );
 }
