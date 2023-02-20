0ce1566bf4d9a5224709837ef76d5f1b92cda658
Noro <noromeb85@gmail.com>
2023-02-20 16:24:35 +0100

    updated left-bar background-color

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -17,7 +17,7 @@ function Dashboard() {
             <div class="row max-width" >
                 <Header updateMyProp={updateMyProp} />
             </div>
-            <div class="row max-width" style={{ backgroundColor: "#f68c8a", height: "100vh" }}>
+            <div class="row max-width" style={{ backgroundColor: "#282828", height: "100vh" }}>
                 <div class="col-lg-2 left-fixed" >
                     <LeftBar />
                 </div>
