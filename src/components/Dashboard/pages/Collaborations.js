8ca53f4d7b98ea26c24546fb16b5302eac974ff0
Noro <noromeb85@gmail.com>
2023-02-21 14:29:23 +0100

    implemented getCollab

--- src/components/Dashboard/pages/Collaborations.js
+++ src/components/Dashboard/pages/Collaborations.js
@@ -1,29 +1,48 @@
 import PaidPromotion from "./../../../chain-info/contracts/PaidPromotion.json"
+import { useState } from "react"
+import { useEthers } from "@usedapp/core";
+import { Contract as CTR } from 'ethers';
 
 function Collaborations() {
 
-    const paidPromotionAddress = "0x30401029AfD9f2835260886c2E1aF2359ff77d4C"
+    const paidPromotionAddress = "0x35B520F9BE399b6Ca2686eF46eeFE484Dc741B9C"
     const { abi } = PaidPromotion
 
-    const handleClick = (event) => {
-        event.preventDefault();
+
+    const [inputValue, setInputValue] = useState();
+
+    const [collab, setCollab] = useState();
+
+    const { account, library } = useEthers();
+
+    const getTokenBalance = async () => {
+        const signer = library.getSigner();
+        const ctr = new CTR(paidPromotionAddress, abi, signer);
+        let tx = await ctr.getCollab(inputValue);
+        setCollab(tx)
     }
+
+    const handleSubmit = (event) => {
+        getTokenBalance();
+        console.log(collab)
+
+        event.preventDefault();
+
+    };
     return (
 
-        <form style={{ width: '600px' }}>
+        <form onSubmit={handleSubmit} style={{ width: '600px' }}>
 
 
             <div class="input-group mb-3">
                 <span class="input-group-text">ID</span>
-                <input type='number' class="form-control" step={1} min="0" />
+                <input type='number' class="form-control" step={1} min="0" value={inputValue}
+                    onChange={(e) => setInputValue(e.target.value)} />
             </div>
             <div class="d-flex justify-content-center ">
 
-
-
-                <button type="submit" class="btn btn-primary create-collab-button mt-4" onClick={handleClick}>See Collab</button>
+                <button type="submit" class="btn btn-primary create-collab-button mt-4" >See Collab</button>
             </div>
-
 
 
         </form>
