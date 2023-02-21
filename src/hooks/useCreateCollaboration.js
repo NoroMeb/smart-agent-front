9871f475f413b78e68ec1325665d7ee9929dfb98
Noro <noromeb85@gmail.com>
2023-02-21 14:30:22 +0100

    updated contract address

--- src/hooks/useCreateCollaboration.js
+++ src/hooks/useCreateCollaboration.js
@@ -4,13 +4,16 @@ import PaidPromotion from "./../chain-info/contracts/PaidPromotion.json";
 import { useContractFunction } from "@usedapp/core";
 
 
+const paidPromotionAddress = "0x35B520F9BE399b6Ca2686eF46eeFE484Dc741B9C"
+const { abi } = PaidPromotion
+const paidPromotionInterface = new ethers.utils.Interface(abi)
+const paidPromotionContract = new Contract(paidPromotionAddress, paidPromotionInterface)
+
 
 export const useCreateCollaboration = () => {
 
-    const paidPromotionAddress = "0x30401029AfD9f2835260886c2E1aF2359ff77d4C"
-    const { abi } = PaidPromotion
-    const paidPromotionInterface = new ethers.utils.Interface(abi)
-    const paidPromotionContract = new Contract(paidPromotionAddress, paidPromotionInterface)
+
     const { state, send } = useContractFunction(paidPromotionContract, "startACollab", {});
     return { state, send };
 }
+
