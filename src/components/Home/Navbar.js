ab3e0d529fb713a4f7f480c0cb8f49f553cde500
Noro <noromeb85@gmail.com>
2023-02-26 13:21:07 +0100

    navbar

--- src/components/Home/Navbar.js
+++ src/components/Home/Navbar.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { Link } from "react-router-dom";
 function NavBar() {
 
     const [isNavCollapsed, setIsNavCollapsed] = useState(true);
@@ -7,9 +8,10 @@ function NavBar() {
 
 
     return (
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" >
+        // <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top home-navbar" >
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark  home-navbar" >
             <div class="container">
-                <a class="navbar-brand" href="#">
+                <a class="navbar-brand home-navbar-brand" href="#">
                     Smart Agent
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded={!isNavCollapsed ? true : false} aria-label="Toggle navigation" onClick={handleNavCollapse}>
@@ -18,10 +20,10 @@ function NavBar() {
                 <div class={`${isNavCollapsed ? 'collapse' : ''} navbar-collapse`} id="navbarSupportedContent">
                     <ul class="navbar-nav ms-auto">
                         <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                            <a class="nav-link  how-it-works" aria-current="page" href="#how-it-works-div">How it works</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#">Link</a>
+                            <Link to={"/dashboard"} target="_blank"><a class="nav-link start-app" target="_blank">Start app</a></Link>
                         </li>
                     </ul>
                 </div>
